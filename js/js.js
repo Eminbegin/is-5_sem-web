8871fe22ed9e064038fda7c7a03dccdf9c22219b
Эмин <eminbegin@gmail.com>
2023-12-29 15:30:25 +0300

    .

--- js/js.js
+++ js/js.js
@@ -6,3 +6,18 @@
         loadTimeSpan.innerHTML += `Page loaded in ${Math.floor(100000 * loadTime) / 100} ms.`;
     });
 })();
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Добавление обработчика событий для каждой навигационной кнопки
+    document.querySelectorAll('.nav-button').forEach(function(button) {
+        button.addEventListener('mouseover', function() {
+            // Действия при наведении на кнопку
+            button.style.boxShadow = '0 0 10px orange';
+        });
+
+        button.addEventListener('mouseout', function() {
+            // Действия при убирании курсора с кнопки
+            button.style.boxShadow = 'none';
+        });
+    });
+});
